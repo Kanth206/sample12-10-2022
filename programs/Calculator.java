580baa559687c35ae1f196de2d62148fcc7e56ae
vchandrakanth206 <vchandrakanth206@gmail.com>
2022-10-12 16:47:20 +0530

    first programs was changed

--- programs/Calculator.java
+++ programs/Calculator.java
@@ -1,45 +1,48 @@
 package programs;
+
 import java.util.Scanner;
 
 public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("enter num1");
-		Scanner sc= new Scanner (System.in);
-		int num1= sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
 		System.out.println("enter num2");
-		Scanner sc1= new Scanner (System.in);
-		int num2= sc1.nextInt();
+		Scanner sc1 = new Scanner(System.in);
+		int num2 = sc1.nextInt();
 		System.out.println("enter operation");
-		Scanner sc2= new Scanner (System.in);
-		char operator=sc2.next().charAt(0);
-		if(operator=='+'||operator=='-'||operator=='*'||operator=='/')
-		{
-		int res=calculator(num1,num2,operator);
-		System.out.println("= "+res);
-		}
-		else {
+		Scanner sc2 = new Scanner(System.in);
+		char operator = sc2.next().charAt(0);
+		if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+			int res = calculator(num1, num2, operator);
+			System.out.println("= " + res);
+		} else {
 			System.out.println("invalid operator");
 		}
-		
 
 	}
-	static int calculator(int a,int b, char operator) {
-		int res=0;
-		switch(operator) {
-		case '+': res=a+b;
-		break;
-		case '-': res=a-b;
-		break;
-		case '*': res=a*b;
-		break;
-		case '/': res=a/b;
-		break;
-			
+
+	static int calculator(int a, int b, char operator) {
+		int res = 0;
+		switch (operator) {
+			case '+':
+				res = a + b;
+				break;
+			case '-':
+				res = a - b;
+				break;
+			case '*':
+				res = a * b;
+				break;
+			case '/':
+				res = a / b;
+				break;
+
 		}
-		
+
 		return res;
 	}
 
-	
 }
+// enter to get operation
--- programs/ReverseString.java
+++ programs/ReverseString.java
@@ -6,26 +6,25 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		System.out.println("enter line of text");
-		Scanner sc=new Scanner(System.in);
-		String text=sc.nextLine();
-		String revString=reverseString(text);
+		Scanner sc = new Scanner(System.in);
+		String text = sc.nextLine();
+		String revString = reverseString(text);
 		System.out.println(text);
 		System.out.println(revString);
-		
-		
 
 	}
-	static String reverseString(String text)
-	{
-		String[] words=text.split("  ");
-		String revString=" ";
-		for(String word:words) {
-			StringBuilder sb= new StringBuilder(word);
-			revString=revString+sb.reverse()+" ";
-			
+
+	static String reverseString(String text) {
+		String[] words = text.split("  ");
+		String revString = " ";
+		for (String word : words) {
+			StringBuilder sb = new StringBuilder(word);
+			revString = revString + sb.reverse() + " ";
+
 		}
-		
+
 		return revString;
 	}
 
 }
+// enter strin
